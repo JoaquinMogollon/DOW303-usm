8372a5cf12535f5f3e4567e571c64a82925e1457
joaquin mogollon <joaquin_mogollon@outlook.com>
2023-04-26 11:54:59 -0400

    creacion BD, controlador y post

--- U2/test1/app/Http/Controllers/EquipoController.php
+++ U2/test1/app/Http/Controllers/EquipoController.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Equipo;
+
 class EquipoController extends Controller
 {
     public function index(){
-        return view('equipos.index');
+        $equipos = Equipo::all();
+        return view('equipos.index',compact('equipos'));
+    }
+
+    public function store(Request $request){
+        //dd($request->nombre);
+        $equipo=new Equipo();
+        
+        $equipo ->nombre = $request->nombre;
+        $equipo ->entrenador = $request->entrenador;
+        $equipo ->save();
+        return redirect()->route('equipos.index');
     }
 }
